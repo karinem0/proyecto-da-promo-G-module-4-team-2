139564ad18c7fccf07333ebe3dd9e880021debf8
rebeca-enma <rebeca.enma@gmail.com>
2024-04-11 12:57:57 +0200

    Descripcion columnas

--- codigo/main.py
+++ codigo/main.py
@@ -36,26 +36,47 @@ cambio_media_mediana = "lead_time"
 
 
 #columnas OK
-is_canceled
-stays_in_weekend_nights	
-stays_in_week_nights
-country
+# is_canceled
+# stays_in_weekend_nights	
+# stays_in_week_nights
+# country
 
-#Completar info descriptiva 
-meal
-
+# #Completar info descriptiva 
+# meal
+# reserved_room_type
+# assigned_room_type
 
 #Columnas revisar 
-arrival_date_day_of_month (16 valores) - valores numericos y nombre
-arrival_date_week_number (54 valores) Quiera 0 
-arrival_date_day_of_month (32 valores revisar si es por valor nulo) Quitar 0
+# arrival_date_day_of_month (16 valores) - valores numericos y nombre
+# arrival_date_week_number (54 valores) Quietar 0 
+# arrival_date_day_of_month (32 valores revisar si es por valor nulo) Quitar 0
+# IS_REPEATED_GUEST (cambiar a SI/NO - Nulos desconocidos)
+# PREVIOUS_BOOKINGS_NOT_CANCELED (cambiar int)
+# booking_changes (gestionar nulos mirar relacion con cancelacion)
+# AGENT (cambiar int)
+# days_in_waiting_list (cambiar int)- propuesta politica cancelaciones
+# adr (imputacion nulos)
+# required_car_parking_spaces (imputacion nulos)
+# total_of_special_requests (cambiar a int)
+# reservation_status (gestion nulos)
+# reservation_status_date (gestion datas)
 
-#Preguntar 
-stays_in_weekend_nights (Quitar decimales )
-stays_in_week_nights (Quitar decimales )
-adults	(valores muy altos)
-children (revisar valores altos)
+
+# #Preguntar 
+# stays_in_weekend_nights (Quitar decimales )
+# stays_in_week_nights (Quitar decimales )
+# adults	(valores muy altos)
+# children (revisar valores altos)
+# DISTRIBUTION_CHANNEL
+# PREVIOUS_CANCELLATIONS
+# customer_type
+
+# # Valorar eliminacion 
+# MARKET_SEGMENT (50% nulos) - ligada a DISTRIBUTION_CHANNEL
+# COMPANY (90% Nulos)
+# '0'
 
 
-
+# %%
+df_sin_duplicados[df_sin_duplicados["previous_cancellations"] >10]
 # %%
